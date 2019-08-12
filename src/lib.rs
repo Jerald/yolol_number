12b68905eb7cdf31c16926c269d3eb77fc54cc98
Oscar <osmithsi@sfu.ca>
2019-08-12 14:07:57 -0700

    Changed exports to be more user-friendly

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 // Required for doing saturating_neg and saturating_abs for YololNumber's
 // #![feature(saturating_neg)]
 
-// For checking int parse errors in YololNumber::from_str
-#![feature(int_error_matching)]
-
 // So we don't have _horrible_ trait bounds
 #![feature(trait_alias)]
 
@@ -17,6 +14,6 @@ mod number;
 mod yolol_ops;
 mod consts;
 
-pub use number::YololNumber;
 pub use yolol_ops::YololOps;
+pub type YololNumber = number::YololNumber<i128>;
 
