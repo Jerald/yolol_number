6046677a2d5bbaf9a2ca2f35c52a49d3a30dd680
Oscar <osmithsi@sfu.ca>
2019-08-15 17:17:16 -0700

    Made comments doc comments

--- src/consts.rs
+++ src/consts.rs
@@ -4,6 +4,7 @@ use num_traits::*;
 
 use crate::yolol_ops::YololOps;
 
+/// Traits needed to be the inner backing type behind a YololNumber
 pub trait InnerBounds = 'static + NumBounds + AsPrimitive<Self> + Signed + Bounded + CheckedAdd + CheckedSub + CheckedMul + CheckedDiv + CheckedRem;
 
 // These used to be bounds on an inner, but they should be conditionally checked in the implementation
@@ -12,5 +13,5 @@ pub trait InnerBounds = 'static + NumBounds + AsPrimitive<Self> + Signed + Bound
 /// Traits needed to be an argument to a YololNumber<T>
 pub trait ArgBounds<T: YololOps> = NumBounds + AsPrimitive<T>;
 
-// Traits needed to interact with a YololNumber
+/// Traits needed to interact with a YololNumber
 pub trait NumBounds = Display + Debug + FromStr + Clone + Copy + Eq + PartialEq + Ord + PartialOrd + Num + NumOps + NumCast;
