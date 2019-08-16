78915a28a850f48ecdf6a555b4cd52bd6a167351
Oscar <osmithsi@sfu.ca>
2019-08-15 17:19:50 -0700

    Rejiggered and massively cleaned up FromStr impl

--- src/number/conversions/from_str.rs
+++ src/number/conversions/from_str.rs
@@ -15,7 +15,6 @@ use crate::yolol_ops::YololOps;
 
 mod error;
 use error::FromStrError as Error;
-use error::FromStrErrorKind as ErrorKind;
 
 static YOLOL_NUM_MATCHER_REGEX: &str = r"^(?P<sign>\+|-)?(?P<main>[0-9]+)(?:\.(?P<dec_zero>0*)(?P<dec_num>[0-9]*))?$";
 
@@ -26,72 +25,98 @@ lazy_static! {
 
 impl<T: YololOps> FromStr for YololNumber<T>
 {
-    type Err = Error<T>;
+    type Err = Error;
 
     fn from_str(string: &str) -> Result<Self, Self::Err>
     {
+        // Runs the input through our regex. Verifies structure and
+        // gives captures for important sections
         let captures = YOLOL_NUM_MATCHER.captures(string)
-            .ok_or_else(|| Error::new(ErrorKind::InputVerificationFailure(string.to_owned()), None))?;
+            .ok_or_else(|| Error::InputVerificationFailure)?;
 
-        let sign = captures.name("sign");
-        let main = captures.name("main");
+        // Gets the number of leading zeroes in the decimal digits
+        let dec_zeros = captures.name("dec_zero")
+            .map_or(0, |m| m.as_str().len());
 
-        // Number of leading zeroes in the decimals
-        let dec_zeros = captures.name("dec_zero").map_or(0, |m| m.as_str().len());
-        let dec_num = captures.name("dec_num");
-
-        let sign_num = match sign
+        // Converts the sign capture group into the equivalent number.
+        // Basically signum but from a string
+        let sign_num = match captures.name("sign")
         {
             None => T::one(),
 
             Some(sign) if sign.as_str() == "+" => T::one(),
             Some(sign) if sign.as_str() == "-" => -T::one(),
 
-            Some(sign) => return Err(Error::new(ErrorKind::InvalidSignMatched(sign.as_str().to_owned()), None))
+            Some(_) => return Err(Error::InvalidSignMatched)
         };
 
-        let main_num = match main
+        // Parses all the digits before the decimal point into a number
+        let main_num = match captures.name("main")
         {
             Some(num) => num.as_str().parse::<T>()
-                .or_else(|_| Err(Error::new(ErrorKind::MainDigitsParseError(num.as_str().to_owned()), None)))?,
+                .map_err(|_| Error::MainDigitsParseError)?,
 
-            None => return Err(Error::new(ErrorKind::NoMainDigits(string.to_owned()), None)),
+            None => return Err(Error::NoMainDigits)
         };
 
-        let decimal_num = match dec_num
+        // Parses the digits after the decimal point into the correct number.
+        // This is the most annoying part of parsing a YololNumber from a string...
+        let decimal_num = match captures.name("dec_num")
         {
+            // If there are 4 or more zeros at the start of the decimal digits
+            // then we're out of digits of precision and just want a 0 (aka, no decimal).
             Some(_) if dec_zeros >= 4 => T::zero(),
 
+            // If there are no decimal numbers, then we also just want 0
             Some(num) if num.as_str().is_empty() => T::zero(),
+
+            // Otherwise, we've gotta do some fancy logic to determine the correct
+            // decimal number we want.
             Some(num) => {
-                // How many digits are needed in the final number out of here
+                // How many digits are needed in the final number out of here.
+                // There are 4 total, so 4 minus the leading zeros is how many we need parsed.
                 let nums_we_need = 4 - dec_zeros;
 
-                // If the slice is too short, clamp so we don't panic
+                // If the slice is too short, clamp so we don't panic.
+                //
+                // Basically, if we have more digits in the slice than we need, we only want
+                // to get those we need. Otherwise, we want to get as many as we have. Logically
+                // that value should then be greater than 0 and less than or equal to 4.
                 let slice_len = usize::min(num.as_str().len(), nums_we_need);
-                if !(slice_len > 0 && slice_len <= 4) { return Err(Error::new(ErrorKind::DecimalSliceLenLogicError(slice_len), None)) }
+                if !(slice_len > 0 && slice_len <= 4) { return Err(Error::DecimalSliceLenLogicError) }
 
+                // We know how many digits we're getting, but we still have to make sure they
+                // express the correct value. For example, if we have the input "1.0110" we'd
+                // get the digits "011" from the slice, which then needs to be multiplied to be
+                // the number 110. Otherwise the inner value is incorrect and the decimal is
+                // expressed wrong.
                 let shift = {
+                    // This is how many powers of 10 we need to multiply the decimal value by.
+                    // Logically it follows that it's difference between how many numbers we need
+                    // and how many numbers we're getting.
                     let shift_pow = nums_we_need - slice_len;
-                    if !(shift_pow <= 3) { return Err(Error::new(ErrorKind::DecimalShiftPowLogicError(shift_pow), None)) }
+                    if shift_pow > 3 { return Err(Error::DecimalShiftPowLogicError) }
 
+                    // This is little hack to do 10^n without dealing with stupid type stuff.
+                    // Thanks iterators!
                     let out = (0..shift_pow).fold(1, |a, _| a*10);
-                    T::from(out)
-                        .ok_or_else(|| Error::new(ErrorKind::ShiftConversionFailure(out), None))?
+
+                    T::from(out).ok_or_else(|| Error::ShiftConversionFailure)?
                 };
 
+                // Now that we know how many digits to get and how much to multiply them by,
+                // we extract said number of digits, multiply them by said power, and away we go!
                 num.as_str()[0..slice_len].parse::<T>()
                     .map(|n| n * shift)
-                    .or_else(|_| Err(Error::new(ErrorKind::DecimalDigitsParseError(num.as_str()[0..slice_len].to_owned()), None)))?
+                    .map_err(|_| Error::DecimalDigitsParseError)?
             },
 
+            // If the capture failed for some reason we just throw in a 0
             None => T::zero(),
         };
 
-        // println!("Decimal_num: {}", decimal_num);
-
-        YololNumber::from_split(main_num, decimal_num)
-            .map(|n: YololNumber<T>| YololNumber::from_inner(n.0 * sign_num))
-            .ok_or_else(|| Error::new(ErrorKind::FromSplitCreationFailure(main_num, decimal_num), None))
+        // Finally construct the final YololNumber, multiplying the main num by the sign
+        YololNumber::<T>::from_split(main_num * sign_num, decimal_num)
+            .ok_or_else(|| Error::FromSplitCreationFailure)
     }
 }
--- src/number/conversions/from_str/error.rs
+++ src/number/conversions/from_str/error.rs
@@ -1,85 +1,43 @@
-use std::error::Error;
 use std::fmt;
 
-use std::ops::Deref;
-
-use crate::YololOps;
-
-type SourceOption = Option<Box<dyn Error + 'static>>;
-
 #[derive(Debug)]
-pub struct FromStrError<T: YololOps>
+pub enum FromStrError
 {
-    kind: FromStrErrorKind<T>,
-    source: SourceOption,
+    // Holds the input that failed verification
+    InputVerificationFailure,
+    // Holds the thing that matched as a sign
+    InvalidSignMatched,
+
+    // Holds the main digits that failed to parse
+    MainDigitsParseError,
+    // Holds the input
+    NoMainDigits,
+
+    // Holds the slice_len that broke bounds 
+    DecimalSliceLenLogicError,
+    // Holds the shift power that broke bounds
+    DecimalShiftPowLogicError,
+    // Holds the shift value that couldn't be converted to T
+    ShiftConversionFailure,
+    // Holds the decimal digits that failed to parse
+    DecimalDigitsParseError,
+
+    // Holds the main num and decimal num that failed the creation
+    FromSplitCreationFailure
 }
 
-impl<T: YololOps> FromStrError<T>
-{
-    pub fn new(kind: FromStrErrorKind<T>, source: SourceOption) -> Self
-    {
-        FromStrError {
-            kind,
-            source
-        }
-    }
-
-    pub fn kind(&self) -> &FromStrErrorKind<T>
-    {
-        &self.kind
-    }
-}
-
-impl<T: YololOps> fmt::Display for FromStrError<T>
+impl fmt::Display for FromStrError
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
     {
-        write!(f, "[YololNumber::FromStrError] Kind '{:?}', Source: '{:?}'", self.kind, self.source)
+        write!(f, "[YololNumber::FromStrError] {:?}", self)
     }
 }
 
-impl<T: YololOps> Error for FromStrError<T>
+impl From<FromStrError> for String
 {
-    fn source(&self) -> Option<&(dyn Error + 'static)>
-    {
-        // Turns `Option<Box<dyn Error + 'static>>`
-        // into  `Option<&(dyn Error + 'static)>`
-        self.source.as_ref().map(|e| e.deref())
-    }
-}
-
-impl<T: YololOps> From<FromStrError<T>> for String
-{
-    fn from(input: FromStrError<T>) -> Self
+    fn from(input: FromStrError) -> Self
     {
         input.to_string()
     }
 }
-
-#[derive(Debug)]
-pub enum FromStrErrorKind<T: YololOps>
-{
-    // Holds the input that failed verification
-    InputVerificationFailure(String),
-    // Holds the thing that matched as a sign
-    InvalidSignMatched(String),
-
-    // Holds the main digits that failed to parse
-    MainDigitsParseError(String),
-    // Holds the input
-    NoMainDigits(String),
-
-    // Holds the slice_len that broke bounds 
-    DecimalSliceLenLogicError(usize),
-    // Holds the shift power that broke bounds
-    DecimalShiftPowLogicError(usize),
-    // Holds the shift value that couldn't be converted to T
-    ShiftConversionFailure(u32),
-    // Holds the decimal digits that failed to parse
-    DecimalDigitsParseError(String),
-
-    // Holds the main num and decimal num that failed the creation
-    FromSplitCreationFailure(T, T)
-
-}
-
